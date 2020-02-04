f606fc5c5786438ac87f6eeeb1e8e34cb38b8017
Brent <brenthaas@gmail.com>
2020-02-03 21:56:00 -0800

    make heading component

--- src/components/BusSchedule.js
+++ src/components/BusSchedule.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { BusScheduleContext } from '../BusScheduleStateProvider';
 import Bus from './Bus';
 import NewBus from './NewBus';
+import ScheduleHeading from './ScheduleHeading';
 
 const BusSchedule = () => {
   const { state } = useContext(BusScheduleContext);
@@ -9,8 +10,7 @@ const BusSchedule = () => {
   if (state) {
     return (
       <div className='bus-schedule'>
-        <div>Bus Schedule:</div>
-        <hr/>
+        <ScheduleHeading/>
         { state.busses.map(bus => <Bus bus={bus} key={`BUS-${bus.id}`}/> )}
         { state.selectedTripId ? <NewBus/> : '' }
       </div>
---
+++ src/components/ScheduleHeading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ScheduleHeading = () => {
+  return (
+    <div className='heading'>Bus Schedule</div>
+  );
+}
+
+export default ScheduleHeading;
